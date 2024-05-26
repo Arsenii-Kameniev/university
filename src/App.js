0050e57c2cde80ed13eca010c6fd90672e717df4
RodionKameniev <kamenev.r.r@gmail.com>
2024-05-26 23:18:12 +0200

    Add Planning Part 1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LecturerInfo from './Department/LecturerInfo/LecturerInfo';
 import { News } from './Department/News/News';
 import { LessonsSchedule } from './Department/LessonsScedule/LessonsSchedule';
 import { Students } from './Department/Students/Students';
+import Planning from './Department/Planning/Planning';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path='/news' element={<News />}></Route>
           <Route path='/students' element={<Students />}></Route>
           <Route path='/lecturer/:lecturerId' element={<LecturerInfo />}></Route>
+          <Route path='/planning' element={<Planning/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
