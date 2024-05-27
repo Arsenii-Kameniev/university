60889880f494952e4578bb51c881c261c959dc50
RodionKameniev <kamenev.r.r@gmail.com>
2024-05-27 19:30:40 +0200

    New Update FOr Plan

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { Main } from './app/Student/Main/Main';
 import LecturerList from './Department/LecturerInfo/LecturerList';
 import LecturerInfo from './Department/LecturerInfo/LecturerInfo';
 import { LessonsSchedule } from './Department/LessonsScedule/LessonsSchedule';
-import { Students } from './Department/Students/Students';
-import Planning from './Department/Planning/Planning';
 import AddPlan from './Department/Planning/AddPlan';
 
 
@@ -18,10 +16,8 @@ function App() {
           <Route path='/' element={<Main />}></Route>
           <Route path='/lecturer-list' element={<LecturerList />}></Route>
           <Route path='/lessons-schedule' element={<LessonsSchedule />}></Route>
-          <Route path='/students' element={<Students />}></Route>
           <Route path='/lecturer/:lecturerId' element={<LecturerInfo />}></Route>
-          <Route path='/planning' element={<Planning/>}></Route>
-          <Route path='/planning/add-plan' element={<AddPlan/>}></Route>
+          <Route path='/planning' element={<AddPlan/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
